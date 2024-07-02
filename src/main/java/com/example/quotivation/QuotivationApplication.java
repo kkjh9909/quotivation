46027a3eb228946d1c79188b66f2e32c63d8436c
kkjh9909 <kkjh990957@gmail.com>
2024-07-02 23:00:44 +0900

    initial commit

---
+++ src/main/java/com/example/quotivation/QuotivationApplication.java
@@ -0,0 +1,13 @@
+package com.example.quotivation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotivationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuotivationApplication.class, args);
+    }
+
+}
